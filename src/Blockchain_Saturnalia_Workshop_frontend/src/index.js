dea82be6ec0e49172ab8c0d25d65467927b099ba
Ansh Kataria <142838226+anshkataria@users.noreply.github.com>
2023-11-22 13:08:05 +0000

    changes

---
+++ src/Blockchain_Saturnalia_Workshop_frontend/src/index.js
@@ -0,0 +1,62 @@
+import { Blockchain_Saturnalia_Workshop_backend } from "../../declarations/todo_backend";
+
+const textarea = document.querySelector(".textarea");
+const appointForm = document.querySelector(".inputcol");
+
+appointForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const obj = {
+    textarea: textarea.value,
+  };
+
+  try {
+    const getId = await Blockchain_Saturnalia_Workshop_backend.addTodo(obj.textarea);
+    showUserOnScreen({ id: getId, textarea: obj.textarea });
+    textarea.value = ""; 
+  } catch (error) {
+    console.error("Error adding todo_Workshop:", error);
+  }
+});
+
+window.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const getAll = await Blockchain_Saturnalia_Workshop_backend.getAllTodo();
+    getAll.forEach((todo) => showUserOnScreen(todo));
+  } catch (error) {
+    console.error("Error fetching todo list:", error);
+  }
+});
+
+function showUserOnScreen(user) {
+  const parentNode = document.getElementById("todolist");
+
+  const newListItem = document.createElement("li");
+  newListItem.className = "item";
+  newListItem.id = Number(user.id);
+  newListItem.textContent = user.textarea;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.className = "trash-button";
+  deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+  deleteButton.onclick = () => deleteTodo(user.id);
+
+  newListItem.appendChild(deleteButton);
+  parentNode.appendChild(newListItem);
+}
+
+async function deleteTodo(userId) {
+  try {
+    console.log("userId in del =>", userId);
+    await Blockchain_Saturnalia_Workshop_backend.deleteTodo(userId);
+    removeUserFromScreen(userId);
+  } catch (error) {
+    console.error("Error deleting todo:", error);
+  }
+}
+
+function removeUserFromScreen(userId) {
+  const childNodeToBeDeleted = document.getElementById(userId);
+  if (childNodeToBeDeleted) {
+    childNodeToBeDeleted.parentNode.removeChild(childNodeToBeDeleted);
+  }
+}
